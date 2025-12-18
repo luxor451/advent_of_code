60b41c46d60bdbf26daa777405d5e8c722120353
di.doche <dimitry.doche2@isae-supaero.com>
2025-12-18 14:27:37 +0100

    formatting

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs;
 use std::time::Instant;
 
-
 fn read_file(path: &str) -> Vec<Vec<String>> {
     let string: String = fs::read_to_string(path).unwrap();
     let content: Vec<String> = string
@@ -52,7 +51,7 @@ fn read_file_two(path: &str) -> Vec<Vec<char>> {
     return content;
 }
 
-fn get_max_lenght_of_number(operande_line : &Vec<char>) -> usize {
+fn get_max_lenght_of_number(operande_line: &Vec<char>) -> usize {
     let mut res = 1;
     while operande_line[res] == ' ' {
         res += 1;
@@ -60,30 +59,37 @@ fn get_max_lenght_of_number(operande_line : &Vec<char>) -> usize {
     return res - 1;
 }
 
-fn func(content : &Vec<Vec<char>>, j : usize, m: usize, n : usize, elem_index : usize) -> u64{
+fn func(content: &Vec<Vec<char>>, j: usize, m: usize, n: usize, elem_index: usize) -> u64 {
     let mut current_nb_vec: Vec<char> = Vec::new();
     for i in 0..(n - 1) {
         current_nb_vec.push(content[i][j + elem_index * m]);
     }
-    let current_nb_vec: Vec<char> = current_nb_vec.into_iter().filter(|x| *x != ' ').collect::<Vec<char>>();
-    let current_number: u64 = current_nb_vec.iter().collect::<String>().parse::<u64>().unwrap();
+    let current_nb_vec: Vec<char> = current_nb_vec
+        .into_iter()
+        .filter(|x| *x != ' ')
+        .collect::<Vec<char>>();
+    let current_number: u64 = current_nb_vec
+        .iter()
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
     return current_number;
 }
 
 fn part_two() {
     let content: Vec<Vec<char>> = read_file_two("inputs.txt");
     let n: usize = content.len();
-    let max_nb_lenght = get_max_lenght_of_number(&content[n-1]);
-    let m: usize = content[0].len() / (max_nb_lenght+1) + 1;
+    let max_nb_lenght = get_max_lenght_of_number(&content[n - 1]);
+    let m: usize = content[0].len() / (max_nb_lenght + 1) + 1;
     let mut res: u64 = 0;
     for elem_index in 0..m {
         let mut res_collumn = func(&content, 0, m, n, elem_index);
-        for j in 1..(m-1) {
-            let current_nb = func(&content, j ,m, n, elem_index);
-            if content[n-1][elem_index * m] == '+' {
+        for j in 1..(m - 1) {
+            let current_nb = func(&content, j, m, n, elem_index);
+            if content[n - 1][elem_index * m] == '+' {
                 res_collumn += current_nb;
             }
-            if content[n-1][elem_index * m] == '*' {
+            if content[n - 1][elem_index * m] == '*' {
                 res_collumn *= current_nb;
             }
         }
